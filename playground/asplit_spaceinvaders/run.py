ac5f3530b39d112852ec62310b0f2c80f08f4a6f
frederikgramkortegaard <frederikgramkortegaard@gmail.com>
2024-04-16 14:40:42 +0300

    Test Github Action black-formatter

--- playground/asplit_spaceinvaders/run.py
+++ playground/asplit_spaceinvaders/run.py
@@ -11,6 +11,7 @@ sys.path.append(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 )
 
+
 # Meta
 from kish.classes import Reinforcement
 from kish.graphing import live_report_reinforcement_agent
